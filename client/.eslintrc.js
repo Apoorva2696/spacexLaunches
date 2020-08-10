a2199becb8b78d41136fa40e9d37841cf898ab38
Apoorva Srivastava <asrivastava@WellDyneRx.com>
2020-08-10 07:54:27 +0530

    final build

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jest": true
+    },
     "extends": [
-      // ...
+      "eslint:recommended",
       "plugin:react-hooks/recommended"
-    ]
+    ],
+    "plugins": [
+      "react"
+  ]
   };
